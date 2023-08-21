d637056a4f63509fb2bfcfcc94b0688a9ccb1a33
Buro <you@example.com>
2023-08-21 14:10:21 +0300

    Learned TS basics and I will continue with TS documents

---
+++ src/index.ts
@@ -0,0 +1,26 @@
+/* class User {
+     public email: string
+    name: string
+    private readonly city: string = 'Ankara'
+    constructor(email: string, name: string) {
+        this.email = email;
+        this.name = name;
+    }
+}
+
+const buro = new User('buro@gmail.com','Buro'); */
+
+
+class User {
+     public email: string
+    name: string
+    private readonly city: string = 'Ankara'
+    constructor(email: string, name: string) {
+        this.email = email;
+        this.name = name;
+    }
+}
+
+const buro = new User('buro@gmail.com','Buro');
+
+
